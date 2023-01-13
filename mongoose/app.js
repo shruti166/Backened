a02448724aa0be290752763876f5cc7b900475c0
shruti166 <shruti.chemistry.lm@gmail.com>
2023-01-13 22:02:50 +0530

    added

--- mongoose/app.js
+++ mongoose/app.js
@@ -1,44 +1,13 @@
 const mongoose = require('mongoose');
-const express = require('express');
-mongoose.set('strictQuery', true);
-const app = express();
-app.use(express.json());
-app.listen(3010);
-
-const db_link = 'mongodb+srv://shruti31:1Shruti1@cluster0.pturoip.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(db_link).then(function(db){
-    console.log("db connected");
-}).catch(function(err) {
-    console.log(err);
-})
 
 
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
+const connect = async() => {
+    try {
+        const connection = mongoose.connect("mongodb://127.0.0.1:27017/")
+        console.log("connected to db");
+    } catch(err) {
+        console.log("cannot connect to database");
+        console.log(err);
     }
-});
-
-//model
-
-const userModel = mongoose.model('userModel', userSchema);
-
-// (async function createUser() {
-//     let user = {
-//         name: 'Shruti',
-//         email: 'abc@gmail.com',
-//         password: '123',
-//         minLength:8
-//     }
-//     let data = await userModel.create(user);
-//     console.log(data);
-// })();
+}
+connect();
