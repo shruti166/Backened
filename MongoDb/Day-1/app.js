8e40c6c3a336e51e03af8415b972ab9ed0e68698
shruti166 <shruti.chemistry.lm@gmail.com>
2023-01-18 10:56:44 +0530

    added

--- MongoDb/Day-1/app.js
+++ MongoDb/Day-1/app.js
@@ -1,5 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 const connect = () => {
-    mongoose.connect(' mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1')
-}
+  mongoose.connect(
+    "mongodb+srv://shruti166:pw05_166@cluster0.q022tkn.mongodb.net/mydb?retryWrites=true&w=majority"
+  );
+  console.log("connected to db");
+};
+connect();
+
+const heroSchema = {
+  name: String,
+  city: String,
+  power: Number,
+  villian: String,
+  language: String,
+};
+
+const HeroModel = mongoose.model("hero", heroSchema);
+
+module.exports = {
+  connect,
+  HeroModel,
+};
